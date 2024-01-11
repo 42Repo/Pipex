84729e618b428f02170029543250341338a4d14d
asuc <sucaurel@gmail.com>
2024-01-11 20:00:59 +0100

    Base Project

---
+++ pipex.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asuc <asuc@student.42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 19:44:39 by asuc              #+#    #+#             */
+/*   Updated: 2024/01/11 19:49:57 by asuc             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "Libft/libft.h"
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+
+
+
+
+#endif
